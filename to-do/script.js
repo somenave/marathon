068c3bb20c46b48527a55e7882e2f4767ba56a7b
ksenia <devqwerty7896@protonmail.com>
2021-11-14 12:27:14 +0300

    с использованием массива

--- to-do/script.js
+++ to-do/script.js
@@ -1,31 +1,94 @@
-const list = {
-    "create a task": "In progress",
-    "make a bed": "Done",
-    "write a post": "To Do"
-};
+const list = [{
+        id: 1,
+        name: 'create a post',
+        status: 'TODO',
+        priority: 'low'
+    },
+    {
+        id: 2,
+        name: 'test',
+        status: 'Done',
+        priority: 'high'
+    }
+];
 
-function changeStatus(task, status="To Do") {
-    if (task && task in list) {
-        list[task] = status;
+function addTask(taskName, taskStatus = 'TODO', priority = 'low') {
+    if (taskName) {
+        list.push({
+            id: list.length + 1,
+            name: taskName,
+            status: taskStatus,
+            priority: priority
+        });
     } else {
-        console.log('You do not have this task yet');
+        console.log('Type task name');
     }
 }
 
-function addTask(task, status="To Do") {
-    if (task) {
-        list[task] = status;
-    } else {
-        console.log('You do not add task-name');
+function changeStatus(taskName, newStatus) {
+    if (taskName) {
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].name == taskName) {
+                list[i].status = newStatus;
+            }
+        }
     }
 }
 
-function deleteTask(task) {
-    if (task && task in list) {
-        delete list[task];
-    } else {
-        console.log("You do not add task-name or there is no such task in yout todo-list");
+function deleteTask(taskName) {
+    if (taskName) {
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].name == taskName) {
+                list.splice(i, 1);
+            }
+        }
     }
 }
+
+addTask('newTask');
+
+changeStatus('newTask', 'Doing');
+
+deleteTask('create a post');
+
+
+//НЕ РАБОТАЕТ
+
+// function showBy(param = 'priority') {
+//     let answer = {};
+//     for (let i = 0; i < list.length; i++) {
+//         if (param == 'priority') {
+//             answer = {
+//                 high: '',
+//                 low: ''
+//             };
+            
+//             if (list[i].priority == 'high') {
+//                 answer.high += (list[i].name);
+//             } else if (list[i].priority == 'low') {
+//                 answer.low += (list[i].name);
+//             }
+//         }
+
+//         if (param == 'status') {
+//             answer = {
+//                 todo: [],
+//                 done: [],
+//                 doing: []
+//             };
+//             if (list[i].status == 'TODO') {
+//                 answer.todo.push(list[i].name);
+//                 console.log('todo');
+//             } else if (list[i].status == 'Done') {
+//                 answer.done.push(list[i].name);
+//             } else if (list[i].status == 'Doing') {
+//                 answer.doing.push(list[i].name);
+//             }
+//         }
+//     }
+//     console.log(answer);
+// }
+
+// showBy('priority');
 
 console.log(list);
