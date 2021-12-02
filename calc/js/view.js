8129dcc7c3f14172a6f8eb4534ffeeda93434f5e
ksenia <devqwerty7896@protonmail.com>
2021-12-02 22:52:40 +0300

    add module in calculator, start ui-todo

---
+++ calc/js/view.js
@@ -0,0 +1,7 @@
+export const resetAllBtn = document.querySelector('.calc-reset__all');
+export const resetSomeBtn = document.querySelector('.calc-reset__some');
+export const actions = document.querySelector('#actions');
+export const equalBtn = document.querySelector('#equal');
+export let result = document.querySelector('.calc-result');
+// export let resultValue = result.textContent; Вот так нельзя
+export const numbers = document.querySelectorAll('.calc-numbers__item');
